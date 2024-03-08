cc294d269d96078f3bf02bb1b5736ec04d997787
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-03-08 22:09:04 +0530

    Update 07-video from web Camera.py

--- 07-video from web Camera.py
+++ 07-video from web Camera.py
@@ -1,18 +1,8 @@
-import cv2 
-cap = cv2.VideoCapture(0) 
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) 
-fps = cap.get(cv2.CAP_PROP_FPS) 
-path = "0" 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-output = cv2.VideoWriter(path, fourcc, 2,(width, height)) 
+import cv2
+video_capture = cv2.VideoCapture(0)
 while True:
-    ret, frame = cap.read()
-    cv2.imshow("frame", frame)
-    output.write(frame)
-    k = cv2.waitKey(24)
-    if k == ord("q"):
-        break 
-cap.release() 
-output.release() 
+    ret, frame = video_capture.read()
+    cv2.imshow('Video Capture', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 cv2.destroyAllWindows()
