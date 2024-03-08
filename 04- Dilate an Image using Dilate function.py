6d29bb6feccf63c7e2806bd32bd1e3b3b5764381
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-03-08 21:51:53 +0530

    Update 04- Dilate an Image using Dilate function.py

--- 04- Dilate an Image using Dilate function.py
+++ 04- Dilate an Image using Dilate function.py
@@ -1,13 +1,8 @@
-import cv2 
-import numpy as np 
-kernel = np.ones((5,5),np.uint8) 
-print(kernel) 
-path = "C:\cv\example 4.jpg"
-img =cv2.imread(path) 
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
-imgBlur = cv2.GaussianBlur(imgGray,(7,7),0) 
-imgCanny = cv2.Canny(imgBlur,100,200) 
-imgDilation = cv2.dilate(imgCanny,kernel , iterations = 10) 
-imgEroded = cv2.erode(imgDilation,kernel,iterations=2) 
-cv2.imshow("Img Erosion",imgEroded) 
-cv2.waitKey(0)
+import cv2
+import numpy as np
+image = cv2.imread("C:\cv\example 5.jpeg")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+kernel = np.ones((5, 5), np.uint8) 
+dilated_image = cv2.dilate(gray_image, kernel, iterations=1)
+cv2.imshow('Dilated Image', dilated_image)
+
