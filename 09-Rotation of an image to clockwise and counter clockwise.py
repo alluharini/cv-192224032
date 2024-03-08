44d0635e236690b4d9035bf4d06b3378bdbab31e
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-03-08 22:21:09 +0530

    Update 09-Rotation of an image to clockwise and counter clockwise.py

--- 09-Rotation of an image to clockwise and counter clockwise.py
+++ 09-Rotation of an image to clockwise and counter clockwise.py
@@ -1,7 +1,4 @@
-import cv2 
-path ="C:\cv\example 2.jpg"
-src = cv2.imread(path) 
-window_name = 'Image' 
-image = cv2.rotate(src, cv2.ROTATE_180) 
-cv2.imshow(window_name, image) 
-cv2.waitKey(0) 
+import cv2
+image = cv2.imread("C:\cv\example 5.jpeg")
+clockwise_image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+cv2.imshow('Rotated Image', clockwise_image)
