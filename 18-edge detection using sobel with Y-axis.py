0c9276426db8a706670541ac8769cbc94465462e
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-03-08 22:45:52 +0530

    Update 18-edge detection using sobel with Y-axis.py

--- 18-edge detection using sobel with Y-axis.py
+++ 18-edge detection using sobel with Y-axis.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 image = cv2.imread("C:\cv\example 5.jpeg")
-sobel_x = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
-sobel_x = cv2.convertScaleAbs(sobel_x)
-cv2.imshow('Sobel X-axis Edge Detection', sobel_x)
+sobel_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
+sobel_y = cv2.convertScaleAbs(sobel_y)
+cv2.imshow('Sobel y-axis Edge Detection', sobel_y)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
