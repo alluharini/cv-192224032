3e70b9c9d874c25192581e4b5883d1c6493bc7a8
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-03-08 21:52:39 +0530

    Update 03-image to show outline using canny function.py

--- 03-image to show outline using canny function.py
+++ 03-image to show outline using canny function.py
@@ -1,11 +1,6 @@
-import cv2 
-import numpy as np 
-kernel = np.ones((5,5),np.uint8) 
-print(kernel) 
-path = "C:\cv\example 4.jpg"
-img =cv2.imread(path) 
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
-imgBlur = cv2.GaussianBlur(imgGray,(7,7),0) 
-imgCanny = cv2.Canny(imgBlur,100,200) 
-cv2.imshow("Img Canny",imgCanny) 
-cv2.waitKey(0) 
+import cv2
+image = cv2.imread('your_image.jpg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray_image, threshold1=30, threshold2=100) 
+cv2.imshow('Edge-detected Image', edges)
+
